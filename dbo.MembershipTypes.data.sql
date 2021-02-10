e710699f8105d6495b0d617adede8ef1fb9950ae
Rehan Memon <rehan4747@gmail.com>
2021-02-10 17:00:11 +0530

    Excercise showing membershiptype completed

---
+++ dbo.MembershipTypes.data.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (1, 0, 0, 0)
+INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (2, 30, 1, 10)
+INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (3, 90, 3, 15)
+INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (4, 300, 12, 20)
+
