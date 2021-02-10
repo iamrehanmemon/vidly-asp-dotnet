a2f432c4e5733083a4b84a99a6236f358e9e50d0
Rehan Memon <rehan4747@gmail.com>
2021-02-10 19:24:05 +0530

    Fixing Errors

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -30,12 +30,12 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customers = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
-            if (customer == null)
+            if (customers == null)
                 return HttpNotFound();
 
-            return View(customer);
+            return View(customers);
         }
     }
 }
--- dbo.MembershipTypes.data.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (1, 0, 0, 0)
-INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (2, 30, 1, 10)
-INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (3, 90, 3, 15)
-INSERT INTO [dbo].[MembershipTypes] ([Id], [SignUpFee], [DurationInMonths], [DiscountRate]) VALUES (4, 300, 12, 20)
-
