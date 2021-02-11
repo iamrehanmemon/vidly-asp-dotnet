eee1a39769706e56d61b7e1ebd6a46aff83164ea
Rehan Memon <rehan4747@gmail.com>
2021-02-11 15:31:13 +0530

    added movies using forms

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -23,6 +23,19 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         public ViewResult Index()
         {
             //var movies = GetMovies();
@@ -41,7 +54,27 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
 
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
 
 
         /*private IEnumerable<Movie> GetMovies()
