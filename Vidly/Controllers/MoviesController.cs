bc3120493fc5b40775e9bb8ec3e47d48a73fc0b4
Rehan Memon <rehan4747@gmail.com>
2021-02-11 12:13:12 +0530

    Excercise 3 Adding Movies & Display Done

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,32 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 
 namespace Vidly.Controllers 
-{
+{   
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            //var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+
+
+
+        /*private IEnumerable<Movie> GetMovies()
         {
             return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
+             {
+                 new Movie { Id = 1, Name = "Shrek" },
+                 new Movie { Id = 2, Name = "Wall-e" }
+             };
+        }*/
+
         // GET: Movies/Random
-        public ActionResult Random()
+        /*public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
@@ -45,7 +73,7 @@ namespace Vidly.Controllers
             return View(viewModel);
             
 
-        }
+        }*/
 
         /*[Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12))}")]
         public ActionResult ByReleaseDate(int year, int month)
