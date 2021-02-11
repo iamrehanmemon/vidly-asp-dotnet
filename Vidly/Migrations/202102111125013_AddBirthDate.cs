f2503d85ef9a1c6485a56c914698562048ec123d
Rehan Memon <rehan4747@gmail.com>
2021-02-11 17:00:28 +0530

    Fixed Bug of Database Error

---
+++ Vidly/Migrations/202102111125013_AddBirthDate.cs
@@ -0,0 +1,19 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthDate : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthdate", c => c.DateTime());
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
